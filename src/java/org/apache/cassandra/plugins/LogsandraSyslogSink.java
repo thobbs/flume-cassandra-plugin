d8216847f553dd2f7d4ecaee1a0dbd770bda754e
Tyler Hobbs <tyler@riptano.com>
2010-09-14 14:40:54 -0500

    Added LogsandraSyslogSink.

---
+++ src/java/org/apache/cassandra/plugins/LogsandraSyslogSink.java
@@ -0,0 +1,201 @@
+package org.apache.cassandra.plugins;
+
+import java.io.IOException;
+import java.util.Calendar;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.TimeZone;
+
+import org.apache.cassandra.thrift.Clock;
+import org.apache.cassandra.thrift.Column;
+import org.apache.cassandra.thrift.ConsistencyLevel;
+import org.safehaus.uuid.UUID;
+import org.safehaus.uuid.UUIDGenerator;
+
+import com.cloudera.flume.conf.Context;
+import com.cloudera.flume.conf.SinkFactory.SinkBuilder;
+import com.cloudera.flume.core.Event;
+import com.cloudera.flume.core.EventSink;
+import com.cloudera.util.Pair;
+import com.cloudera.flume.handlers.syslog.SyslogConsts;
+
+/**
+ * Uses Cassandra as a Logsandra compatible sink for syslog messages.
+ * 
+ * When the sink receives an event, it does the following:
+ *
+ * 1. Inserts the entry in to the "entries" column family with
+ *    a version 1 UUID as the row key.
+ * 2. Using the "by_date" column family, indexes the entry by
+ *    source, severity, and facility.
+ *
+ * From there, Logsandra should be able to use the data.  You
+ * can find Logsandra at http://github.com/jbohman/logsandra
+ */
+public class LogsandraSyslogSink extends EventSink.Base {
+  
+  private static final String KEYSPACE = "logsandra";
+  private static final String ENTRIES = "entries";
+  private static final String BY_DATE = "by_date";
+  
+  // Column names
+  private static final byte[] IDENT_NAME = "ident".getBytes();
+  private static final byte[] SOURCE_NAME = "source".getBytes();
+  private static final byte[] DATE_NAME = "date".getBytes();
+  private static final byte[] ENTRY_NAME = "entry".getBytes();
+    
+  private CassandraClient cClient;
+
+  private static final UUIDGenerator uuidGen = UUIDGenerator.getInstance();
+
+  private static final long MILLI_TO_MICRO = 1000; // 1ms = 1000us
+
+  public LogsandraSyslogSink(String[] servers) {
+    
+    this.cClient = new CassandraClient(KEYSPACE, servers);
+  }
+
+  @Override
+  public void open() throws IOException {
+    this.cClient.open();
+  }
+
+  /**
+   * Writes the message to Cassandra in a Logsandra compatible way.
+   */
+  @Override
+  public void append(Event event) throws IOException {
+
+    Clock clock = new Clock(System.currentTimeMillis() * MILLI_TO_MICRO);
+
+    UUID uuid = uuidGen.generateTimeBasedUUID();
+    
+    String date = getDate();
+    String host = event.getHost();
+    String facility = SyslogConsts.FACILITY[event.get(SyslogConsts.SYSLOG_FACILITY)[0]];
+    String severity = SyslogConsts.SEVERITY[event.get(SyslogConsts.SYSLOG_SEVERITY)[0]].toString();
+    
+    StringBuffer eventInfo = new StringBuffer();
+    eventInfo.append(date);
+    eventInfo.append(' ');
+    eventInfo.append(host);
+    eventInfo.append(' ');
+    eventInfo.append(facility);
+    eventInfo.append(' ');
+    eventInfo.append(severity);
+    eventInfo.append(' ');
+    
+    byte[] eventBody = event.getBody();
+    byte[] eventInfoBytes = eventInfo.toString().getBytes();
+    byte[] finalBytes = new byte[eventInfoBytes.length + eventBody.length];
+
+    for(int i = 0; i < eventInfoBytes.length; i++) {
+      finalBytes[i] = eventInfoBytes[i];
+    }
+    
+    for(int i = 0; i < eventBody.length; i++) {
+      finalBytes[i + eventInfoBytes.length] = eventBody[i];
+    }
+    
+    Column identColumn = new Column(IDENT_NAME, "ident".getBytes(), clock);
+    Column sourceColumn = new Column(SOURCE_NAME, host.getBytes(), clock);
+    Column dateColumn = new Column(DATE_NAME, date.getBytes(), clock);
+    Column entryColumn = new Column(ENTRY_NAME, finalBytes, clock);
+    Column[] entryColumns = {identColumn, sourceColumn, dateColumn, entryColumn};
+    
+    Long time = System.currentTimeMillis() * MILLI_TO_MICRO;
+    Column timeColumn = new Column(toBytes(time), uuid.toString().getBytes(), clock);
+    Column[] byDateColumns = {timeColumn};
+
+    // Insert the entry
+    this.cClient.insert(uuid.toString().getBytes(), ENTRIES, entryColumns, ConsistencyLevel.QUORUM);
+    // Insert the keyword indexes
+    this.cClient.insert(
+        event.getHost().getBytes(),
+        BY_DATE, byDateColumns, ConsistencyLevel.QUORUM);
+    this.cClient.insert(
+        SyslogConsts.FACILITY[event.get(SyslogConsts.SYSLOG_FACILITY)[0]].getBytes(),
+        BY_DATE, byDateColumns, ConsistencyLevel.QUORUM);
+    this.cClient.insert(
+        SyslogConsts.SEVERITY[event.get(SyslogConsts.SYSLOG_SEVERITY)[0]].toString().getBytes(),
+        BY_DATE, byDateColumns, ConsistencyLevel.QUORUM);
+    
+    super.append(event);
+  }
+
+  /** Converts a Long to a big-endian byte[] */
+  private byte[] toBytes(Long obj) {
+    if (obj == null) {
+      return null;
+    }
+    long l = obj;
+    int size = 8;
+    byte[] b = new byte[size];
+    for (int i = 0; i < size; ++i) {
+      b[i] = (byte) (l >> (size - i - 1 << 3));
+    }
+    return b;
+  }
+
+  /**
+   * Returns a byte[] representing the current date.
+   * This has the format "YYYY-MM-DD HH:MM:SS".
+   */
+  private String getDate() {
+    Calendar cal = Calendar.getInstance(TimeZone.getTimeZone("GMT+0"));
+    int day = cal.get(Calendar.DAY_OF_MONTH);
+    int month = cal.get(Calendar.MONTH);
+    int year = cal.get(Calendar.YEAR);
+    int hour = cal.get(Calendar.HOUR_OF_DAY);
+    int minute = cal.get(Calendar.MINUTE);
+    int second = cal.get(Calendar.SECOND);
+
+    StringBuffer buff = new StringBuffer();
+    buff.append(year);
+    if(month < 10)
+      buff.append('0');
+    buff.append(month);
+    if(day < 10)
+      buff.append('0');
+    buff.append(day);
+    if(hour < 10)
+      buff.append('0');
+    buff.append(hour);
+    if(minute < 10)
+      buff.append('0');
+    buff.append(minute);
+    if(second < 10);
+      buff.append('0');
+    buff.append(second);
+    return buff.toString();
+  }
+
+  @Override
+  public void close() throws IOException {
+    this.cClient.close();
+  }
+
+  public static SinkBuilder builder() {
+    return new SinkBuilder() {
+      @Override
+      public EventSink build(Context context, String ... args) {
+        if (args.length < 1) {
+          throw new IllegalArgumentException(
+              "usage: logsandraSyslogSink(\"host:port\"...");
+        }
+        return new LogsandraSyslogSink(args);
+      }
+    };
+  }
+
+  /**
+   * This is a special function used by the SourceFactory to pull in this class
+   * as a plugin sink.
+   */
+  public static List<Pair<String, SinkBuilder>> getSinkBuilders() {
+    List<Pair<String, SinkBuilder>> builders =
+      new ArrayList<Pair<String, SinkBuilder>>();
+    builders.add(new Pair<String, SinkBuilder>("logsandraSyslogSink", builder()));
+    return builders;
+  }
+}
--- src/java/org/apache/cassandra/plugins/SimpleCassandraSink.java
+++ src/java/org/apache/cassandra/plugins/SimpleCassandraSink.java
@@ -3,20 +3,11 @@ package org.apache.cassandra.plugins;
 import java.io.IOException;
 import java.util.Calendar;
 import java.util.ArrayList;
-import java.util.LinkedList;
-import java.util.Queue;
 import java.util.Arrays;
 import java.util.List;
-import java.util.Random;
 import java.util.TimeZone;
 
 import org.apache.cassandra.thrift.*;
-import org.apache.thrift.TException;
-import org.apache.thrift.protocol.TBinaryProtocol;
-import org.apache.thrift.protocol.TProtocol;
-import org.apache.thrift.transport.TFramedTransport;
-import org.apache.thrift.transport.TSocket;
-import org.apache.thrift.transport.TTransport;
 
 import org.safehaus.uuid.UUID;
 import org.safehaus.uuid.UUIDGenerator;
@@ -39,66 +30,27 @@ import com.cloudera.util.Pair;
  * SimpleCassandraSink primarily targets log storage right now.
  */
 public class SimpleCassandraSink extends EventSink.Base {
-
-  private ServerSet serverSet;
-  private String currentServer;
-  
-  private String keyspace;
   
   private String dataColumnFamily;
-  private ColumnParent dataColumnParent;
   private String indexColumnFamily;
-  private ColumnParent indexColumnParent;
-  
-  private Cassandra.Client client;
-  private TTransport transport;
+    
+  private CassandraClient cClient;
 
   private static final UUIDGenerator uuidGen = UUIDGenerator.getInstance();
 
-  /** How long we wait before retrying a dead server. */
-  private static final long DEFAULT_RETRY_TIME = 500; // 500ms
   private static final long MILLI_TO_MICRO = 1000; // 1ms = 1000us
 
   public SimpleCassandraSink(String keyspace, String dataColumnFamily,
       String indexColumnFamily, String[] servers) {
-    this.keyspace = keyspace;
     this.dataColumnFamily = dataColumnFamily;
-    this.dataColumnParent = new ColumnParent(this.dataColumnFamily);
     this.indexColumnFamily = indexColumnFamily;
-    this.indexColumnParent = new ColumnParent(this.indexColumnFamily);
-    this.serverSet = new ServerSet(servers, DEFAULT_RETRY_TIME);
-    this.currentServer = null;
+    
+    this.cClient = new CassandraClient(keyspace, servers);
   }
 
   @Override
   public void open() throws IOException {
-    // Initialize the sink
-    try {
-      this.currentServer = this.serverSet.get();
-    } catch (ServerSet.NoServersAvailableException e) {
-      throw new IOException("No Cassandra servers available.");
-    }
-
-    int splitIndex = this.currentServer.indexOf(':');
-    if(splitIndex == -1) {
-      throw new IOException("Bad host:port pair: " + this.currentServer);
-    }
-    String host = this.currentServer.substring(0, splitIndex);
-    int port = Integer.parseInt(this.currentServer.substring(splitIndex + 1));
-
-    TSocket sock = new TSocket(host, port);
-    this.transport = new TFramedTransport(sock);
-    TProtocol protocol = new TBinaryProtocol(transport);
-    this.client = new Cassandra.Client(protocol);
-
-    try {
-      this.transport.open();
-      this.client.set_keyspace(this.keyspace);
-    } catch(TException texc) {
-      throw new IOException(texc.getMessage());
-    } catch(InvalidRequestException exc) {
-      throw new IOException(exc.getMessage());
-    }
+    this.cClient.open();
   }
 
   /**
@@ -119,17 +71,11 @@ public class SimpleCassandraSink extends EventSink.Base {
     // Make the data column
     Column dataColumn = new Column("data".getBytes(), event.getBody(), clock);
 
-    try {
-      // Insert the index
-      this.client.insert(this.getKey(), this.indexColumnParent, indexColumn, ConsistencyLevel.QUORUM);
-      // Insert the data (row key is the uuid and there is only one column)
-      this.client.insert(uuid.toString().getBytes(), this.dataColumnParent, dataColumn, ConsistencyLevel.QUORUM);
-      super.append(event);
-    } catch (Exception exc) {
-      this.serverSet.markDead(this.currentServer);
-      this.open();
-      this.append(event);
-    }
+    // Insert the index
+    this.cClient.insert(this.getKey(), this.indexColumnFamily, new Column[] {indexColumn}, ConsistencyLevel.QUORUM);
+    // Insert the data (row key is the uuid and there is only one column)
+    this.cClient.insert(uuid.toString().getBytes(), this.dataColumnFamily, new Column[] {dataColumn}, ConsistencyLevel.QUORUM);
+    super.append(event);
   }
 
   /**
@@ -159,7 +105,7 @@ public class SimpleCassandraSink extends EventSink.Base {
 
   @Override
   public void close() throws IOException {
-    this.transport.close();
+    this.cClient.close();
   }
 
   public static SinkBuilder builder() {
@@ -186,74 +132,5 @@ public class SimpleCassandraSink extends EventSink.Base {
       new ArrayList<Pair<String, SinkBuilder>>();
     builders.add(new Pair<String, SinkBuilder>("simpleCassandraSink", builder()));
     return builders;
-  }
-
-  /** Holds a set of servers that may be marked dead and reinstated. */
-  private class ServerSet {
-
-    private ArrayList<String> servers;
-    private int serverIndex;
-    private Queue<Pair> dead = new LinkedList<Pair>();
-    private long retryTime;
-
-    ServerSet(String[] servers, long retryTime) {
-      this.retryTime = retryTime;
-
-      // Uniformly randomly permute the list()
-      Random rand = new Random();
-      for(int i=0; i < servers.length; i++) {
-        int j = rand.nextInt(servers.length);
-        String temp = servers[i];
-        servers[i] = servers[j];
-        servers[j] = temp;
-      }
-
-      this.servers = new ArrayList<String>();
-      for(int i=0; i < servers.length; i++) {
-        this.servers.add(servers[i]);
-      }
-      this.serverIndex = 0;
-    }
-
-    /** Gets the next available server. */
-    String get() throws NoServersAvailableException {
-      if(!this.dead.isEmpty()) {
-        Pair pair = this.dead.remove();
-        if (pair.l > System.currentTimeMillis())
-          this.dead.add(pair);
-        else
-          this.servers.add(pair.str);
-      }
-      if(this.servers.isEmpty()) {
-        throw new NoServersAvailableException();
-      }
-      return this.servers.get(this.serverIndex++);
-    }
-
-    /**
-     * Marks the server as dead.  It will be reinstated after retryTime
-     * has passed.
-     */
-    void markDead(String server) {
-      this.servers.remove(server);
-      this.dead.add(new Pair(server, System.currentTimeMillis() + this.retryTime));
-    }
-
-    private class Pair {
-      String str;
-      Long l;
-
-      Pair(String s, Long l) {
-        this.str = s;
-        this.l = l;
-      }
-    }
-
-    public class NoServersAvailableException extends Exception {      
-      private static final long serialVersionUID = 1L;
-      NoServersAvailableException() {
-        super();
-      }
-    }
   }
 }
